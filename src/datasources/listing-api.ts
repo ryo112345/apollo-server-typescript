d46b0be9e2a4556ef335e4ded0981171142b7ca9
ryo112345 <ryo112345swe@gmail.com>
2025-08-03 13:24:40 +0900

    createListingミューテーションを実装

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,7 +1,8 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { Listing, Amenity } from "../types";
+import { Listing, Amenity, CreateListingInput } from "../types";
 
 export class ListingAPI extends RESTDataSource {
+
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
 
   getFeaturedListings(): Promise<Listing[]> {
@@ -13,7 +14,12 @@ export class ListingAPI extends RESTDataSource {
   }
 
   getAmenities(listingId: string): Promise<Amenity[]> {
-    console.log("Making a follow-up call for amenities with ", listingId);
-    return this.get<Amenity[]>(`listings/${listingId}/amenities`)
+    return this.get<Amenity[]>(`listings/${listingId}/amenities`);
+  }
+
+  createListing(listing: CreateListingInput): Promise<Listing> {
+    return this.post<Listing>("listings", {
+      body: { listing }
+    });
   }
 }
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -16,5 +16,25 @@ export const resolvers: Resolvers = {
         ? amenities
         : dataSources.listingAPI.getAmenities(id);
     }
-  }
+  },
+  Mutation: {
+    createListing: async (_, { input }, { dataSources }) => {
+      try {
+        const response = await dataSources.listingAPI.createListing(input);
+        return {
+          code: 200,
+          success: true,
+          message: "Listing successfully created!",
+          listing: response
+        };
+      } catch (err) {
+        return {
+          code: 500,
+          success: false,
+          message: `Something went wrong: ${err.extensions.response.body}`,
+          listing: null
+        };
+      }
+    },
+  },
 };
